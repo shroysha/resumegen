d84be08f4c64a1af5d41a9a8245bcc8d75934027
Shawn Shroyer <shroysha@gmail.com>
2020-03-22 21:35:35 -0400

    auto

---
+++ resume_info.py
@@ -0,0 +1,42 @@
+"""
+Shawn Shroyer	shroysha@gmail.com
+(240) 310-8658	https://www.shroysha.dev
+
+Education
+PCAP	Certified Associate in Python Programming	2019
+GSEC	GIAC Security Essentials Certification	2015
+CCNA	Cisco Certified Network Associate in Routing and Switching	2015
+ITIL	Axelos Information Technology Infrastructure Library v3 	2015
+Graduate of The Pennsylvania State University 	 	2015
+•	B.S. Information Sciences and Technology Major (Integration of Applications focus)
+•	Security and Risk Analysis Minor (Network Security focus)
+GPA – 3.59, Dean’s List in all semesters, Academic Excellence in Information Sciences and Technology Award
+
+Work Experience (9 years of programming experience – mainly hobby)
+Application Developer
+Independent, Hagerstown, MD	10/2016 – Present
+•	Used IntelliJ to implement Gradle, MVC, Spring Boot, JUnit, and Lombok into my personal coding portfolio
+•	Created a Python modulo 29 number theory module in attempt to solve the 2015 Cicada 3301 puzzle
+•	Created a VR app using C#, Unity, PHP, MySQL, and OAuth2, which displays user-created geographical content, using the devices’ location, gyroscopic angle, and compass data
+
+IT Support Specialist Level 3
+The Pennsylvania State University, State College, PA	01/2016 – 10/2016
+•	Moved to State College (post-graduation) and supported the Eberly College of Science’s Astronomy, Math, and Physics departments Linux research servers and their classroom and office Windows / Mac desktops
+•	Installed, packaged, and resolved issues with Anaconda, GCC, R Server, MATLAB, Maple, Mathematica, ArcGIS, and any other scientific software requested from researchers (some compiled from source)
+•	Lead and managed departments’ projects and assisted other departments as needed (primarily with Mac and Linux)
+•	Developed Bash, Python, and PowerShell scripts to automate daily tasks in SSH, SharePoint, and Active Directory
+NOC Technician
+TE Connectivity, Shirley, NY	09/2015 - 10/2015
+•	Participated in a two week contract to assist the physical setup of a WAN NOC at an AT&T facility
+Service Desk Technician	06/2014 - 09/2015
+National Institute of Standards and Technology (NIST), Gaithersburg, MD
+•	Worked full-time at a highly structured helpdesk providing user support to Gaithersburg and Boulder, while in college
+Student Technician Supervisor
+The Pennsylvania State University, Mont Alto, PA	01/2013 - 06/2014
+•	Received promotion (from Student Technician) on 01/2014
+
+
+please contact for more details and interview
+
+
+"""
